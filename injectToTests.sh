491e1a6aa426c76426f3583618317a1395281edc
denis0001-dev <denis0001.dev@ya.ru>
2025-03-10 23:04:01 +0300

    fixed minifier

--- injectToTests.sh
+++ injectToTests.sh
@@ -3,7 +3,7 @@
 export PATH=$PATH:$HOME/.nvm/versions/node/v22.14.0/bin/
 
 # shellcheck disable=SC2005
-minified="$(./node_modules/.bin/minify webcorrupter.js | xargs)"
+minified="$(./node_modules/.bin/minify webcorrupter.js | xargs | sed -E 's/^\s*use strict/"use strict"/;t')"
 if [ "$minified" == "" ]; then
     echo "ERROR: minify doesn't exist." > /dev/stderr
     exit 1
