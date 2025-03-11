910a0b0d217895f3464c9ff45cdd3ca5827aca83
denis0001-dev <denis0001.dev@ya.ru>
2025-03-11 12:18:19 +0300

    switch to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("multiplatform") version "2.0.21"
+}
+
+group = "ru.morozovit"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+            implementation(kotlin("stdlib-js"))
+            implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.8.0")
+        }
+    }
+}
+
--- injectToTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in tests/*/; do
-    echo "Copying script for $i"
-    cp -f webcorrupter.js "$i"
-done
-
-echo "Success!"
